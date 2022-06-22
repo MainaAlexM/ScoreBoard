ec0a9ee893cb6c447601f86e9c930ac1e1cbd604
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 14:30:37 +1000

    Test: Add profile test class

--- scores/tests.py
+++ scores/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Project,Ratings
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.user=User(
+            username='prof',email='apptestermind@gmail.com',password='key_one'
+    )
+        self.image=Project(title='test',owner=self.user,landing_page='https://www.pexels.com/photo/portrait-of-beautiful-woman-posing-outdoors-12339648/200/300',description='Trial Message',site_url='https://pexels.com')
+
+
+        self.user.save()
+        self.image.save()
+
+
+    def test_save_profile(self):
+        profiles = Profile.objects.all()
+
+        self.assertEquals(len(profiles),1)
+
+
