9803571bcb12beb1bbf12d70f5d95317f723ab1f
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 14:19:05 +1000

    Feat: Add user profile model

--- scores/models.py
+++ scores/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_image = CloudinaryField("profile_image")
+    bio = models.TextField()
+    user = models.OneToOneField(User,  related_name='profiles' ,on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.user.username
+
+    @classmethod
+    def update_profile(cls, username, email, bio, profile_image):
+        cls.update(username=username, email=email,
+                bio=bio, profile_image=profile_image)
+
+    @classmethod
+    def save_profile(cls, profile):
+        cls.save(profile)
+
+    @classmethod
+    def delete_profile(cls, profile):
+        cls.delete(profile)
+
