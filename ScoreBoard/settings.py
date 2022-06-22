22085466ab42df3d11814c72186e47b6ecc38a44
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 14:16:15 +1000

    Config: Edit Database cofiguration settings

--- ScoreBoard/settings.py
+++ ScoreBoard/settings.py
@@ -92,13 +92,19 @@ WSGI_APPLICATION = 'ScoreBoard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+if config('MODE')=='dev':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
+else:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+    }
 
 
 # Password validation
